d02792e3dfc7fd6d4d8fe4b280cc1e663038eadb
Ethan Simpson <ethan_simpson@live.com>
2025-12-23 20:28:01 -0800

    deal with this later

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { cloudflare } from '@cloudflare/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,7 +7,7 @@ export default defineConfig({
   build: {
     assetsInlineLimit: 0,
   },
-  plugins: [react(), cloudflare()],
+  plugins: [react()],
   assetsInclude: [
     '**/*.svg',
     '**/*.ttf',
