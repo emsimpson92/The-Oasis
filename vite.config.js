fd5bd9283a217f1466888167e6866e08751277b2
Ethan Simpson <ethan_simpson@live.com>
2025-12-23 19:57:19 -0800

    debugging build

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
+  build: {
+    assetsInlineLimit: 0,
+  },
   plugins: [react()],
   assetsInclude: [
     '**/*.svg',
